c130c7b3e16d13267408d404bf43b14fb9a3b1ce
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2012-01-19 18:06:41 +0100

    New plugin API

--- build.js
+++ build.js
@@ -1,17 +1,17 @@
 module.exports = {
-	"startfile":function(html,params,callback) {
+  "startfile":function(runtime,params,callback) {
 
-		callback(null,html.replace(/<\/head>/i,
-			'<script type="text/javascript">'
-			+ 'var _gaq = _gaq || [];'
-			+ "_gaq.push(['_setAccount', '"+params.options.accountid+"']);"
-		    + "_gaq.push(['_trackPageview']);"
+    callback(null,params["content"].replace(/<\/head>/i,
+      '<script type="text/javascript">'+
+        'var _gaq = _gaq || [];'+
+        "_gaq.push(['_setAccount', '"+params.options.accountid+"']);"+
+        "_gaq.push(['_trackPageview']);"+
 
-		    + "(function() {"
-		    +  "var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;"
-		    +  "ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';"
-		    +  "var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);"
-		    + "})();"
-			+ "</script></head>"));
-	}
+        "(function() {"+
+          "var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;"+
+          "ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';"+
+          "var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);"+
+        "})();"+
+      "</script></head>"));
+  }
 };
