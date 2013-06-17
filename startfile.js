be80f42d40bac5d905b975717ee4ca822f1d727a
Clément Vollet <clement@joshfire.com>
2013-06-17 16:28:00 +0200

    Double quote > single quote

--- startfile.js
+++ startfile.js
@@ -8,14 +8,14 @@ define([], function () {
     callback(null, params.content.replace(/<\/head>/i,
       '<script type="text/javascript">'+
         'var _gaq = _gaq || [];'+
-        "_gaq.push(['_setAccount', '"+params.options.accountid+"']);"+
-        "_gaq.push(['_trackPageview']);"+
+        '_gaq.push([\'_setAccount\', \''+params.options.accountid+'\']);'+
+        '_gaq.push([\'_trackPageview\']);'+
 
-        "(function() {"+
-          "var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;"+
-          "ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';"+
-          "var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);"+
-        "})();"+
-      "</script></head>"));
+        '(function() {'+
+          'var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;'+
+          'ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';'+
+          'var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);'+
+        '})();'+
+      '</script></head>'));
   };
 });
