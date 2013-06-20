e2ccc752df70a74d756361645b90c5c04263384e
Thibaud Cummings <thibaud.cummings@gmail.com>
2013-06-20 09:52:41 +0200

    cordova GAPlugin integration copied the GAPlugin files from https://github.com/phonegap-build/GAPlugin since we want the JS to be injected in the bootstrap i removed the <asset> property in GAPlugin's plugin.xml added xcodeproj and androidproj hooks which simply invoke plugman.install

---
+++ injectCordovaPluginJS.js
@@ -0,0 +1,27 @@
+define(['woodman'], function (woodman) {
+
+  return function (runtime, params, callback) {
+    var logger = woodman.getLogger('add-on', 'google-analytics', 'injectCordovaPluginJS');
+    logger.log('started');
+
+    var jsFilePath = './GAPlugin/www/GAPlugin.js';
+
+    /**
+     * wrap JS file in IIFE and append it to cordova in bootstrap
+     *
+     * @function
+     * @param {function} cb Callback
+     */
+    runtime.injectJS(jsFilePath, function (err) {
+      if (err) {
+        logger.warn('injectJS error', err);
+      } else {
+        logger.log('done');
+      }
+
+      callback(err);
+      return;
+    });
+
+  };
+});
